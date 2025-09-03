739ee55c061fd782d2af8c7f5a84587052a834fe
yurii-leginchuk <yurij.leginchuk.dev@gmail.com>
2025-09-03 14:14:37 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Installation Guide
+
+## Prerequisites
+- [NodeJS](https://nodejs.org/en/)
+
+## Global Dependencies
+Install the following tools globally:
+```bash
+npm i -g yarn
+npm i -g gulp
+yarn set version berry
+yarn
+yarn run dev
---
+++ src/js/index.js
@@ -0,0 +1,61 @@
+import "./import/modules";
+import "./import/components";
+import "./inputmask.min";
+
+(() => {
+    Inputmask({
+        mask: "+7 (999) 999-99-99",
+        showMaskOnHover: false,
+        clearIncomplete: true
+    }).mask("#tel");
+})();
+
+(() => {
+    const forms = document.querySelectorAll(".gs-form");
+
+    forms.forEach(form => {
+        const fioInput = form.querySelector("input[name='fio']");
+        const telInput = form.querySelector("input[name='tel']");
+
+
+        const validateFio = () => {
+            const value = fioInput.value.trim();
+            return /^[А-Яа-яЁёA-Za-z\s]{2,}$/.test(value);
+        };
+
+        const validateTel = () => {
+            return /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/.test(telInput.value);
+        };
+
+
+        form.addEventListener("submit", (e) => {
+            let valid = true;
+
+            if (!validateFio()) {
+                fioInput.classList.add("error");
+                valid = false;
+            }
+            if (!validateTel()) {
+                telInput.classList.add("error");
+                valid = false;
+            }
+
+            if (!valid) {
+                e.preventDefault();
+
+            }
+        });
+
+        fioInput.addEventListener("input", () => {
+            if (fioInput.classList.contains("error") && validateFio()) {
+                fioInput.classList.remove("error");
+            }
+        });
+
+        telInput.addEventListener("input", () => {
+            if (telInput.classList.contains("error") && validateTel()) {
+                telInput.classList.remove("error");
+            }
+        });
+    });
+})();
